6c0b8ebf04c487df890db3eae0076446a6ffa5fc
Tiberiu Tofan <tibtof@gmail.com>
2020-06-18 20:58:26 +0300

    background video

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
+import "../styles/video.css"
 
 export default function Index() {
   return (
     <Layout>
-      <div>
+      <div className="vimeo-wrapper">
+        <div className="overlay"/>
+        <iframe src="https://player.vimeo.com/video/430445552?background=1&autoplay=1&loop=1&byline=0&title=0"
+                frameBorder="0" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe>
       </div>
     </Layout>
   )
